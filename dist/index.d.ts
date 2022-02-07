189c91f96bb1f30d3929902373530ca9e94bb02f
Ved <tmgbedu@gmail.com>
2022-02-07 18:02:40 +0545

    v0.1.35

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import FlashContainer from "./components/Flash.vue";
-import { FlashPlugin } from "./FlashPlugin";
-import Flash from "./js/Flash";
-export { FlashContainer, FlashPlugin, Flash, };
-//# sourceMappingURL=index.d.ts.map
--- dist/utils/emitter.d.ts
+++
@@ -1,4 +0,0 @@
-import { TinyEmitter } from "tiny-emitter";
-declare const _default: TinyEmitter;
-export default _default;
-//# sourceMappingURL=emitter.d.ts.map
